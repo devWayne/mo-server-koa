e09f592e2323f298ec53b47397d556d66eb41a38
Wayne <dev.wayne.ix@gmail.com>
2014-08-26 10:41:22 +0800

    postajax

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var koa = require('koa'),
 	router = require('koa-router'),
 	logger = require('koa-logger'),
-	mongoose = require('mongoose');
-	app = koa();
-
+	parse = require('co-body');
+mongoose = require('mongoose');
+app = koa();
+app.use(router(app));
+app.use(bodyParser());
 app.use(logger());
-var route = new router();
-route.get('/', function* next() {
-	  console.log(this.params);
-  // => [ category: 'programming', title: 'how-to-node' ]
+
+app.post('/ajax', function* next() {
+	console.log(this.request.method);
+	console.log(this.request.header);
+	
+	var body = yield parse(this.request);
+	console.log(body);
+	this.body = body;
 });
 app.listen(3000);
