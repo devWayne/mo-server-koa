c1e34ddcc3ddaa7b730733d256725799071da783
Wayne <dev.wayne.ix@gmail.com>
2014-08-28 09:42:12 +0800

    route

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var koa = require('koa'),
 	logger = require('koa-logger'),
 	route = require('./route');
-	
+
 var app = module.exports = koa();
 
+//logger
 app.use(logger());
+
+//route
 route(app);
 
 app.listen(3000);
